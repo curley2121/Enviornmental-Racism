c0fb63cfd749669ae4f1bdfce3ef15ea1979da2d
co1010 <colinclarke1010@gmail.com>
2021-04-18 22:57:00 -0400

    Finished implementing skeleton code

---
+++ src/components/ContentCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Content cards are like posts on the instagram project except we need to conditionally
+// render parts of the card depending on the type of content.
+function ContentCard(props) {
+    let content = props;
+
+    return (
+        <p>Content card</p>
+    );
+}
+
+export default ContentCard;
