8cf231e6f2e873a8bcfa853731ec394da97ba25e
Glitch (environmental-racism-prb) <none>
2021-04-26 18:26:02 +0000

    Pat Updates

--- src/components/FullPage.js
+++ src/components/FullPage.js
@@ -1,43 +1,13 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
-import { StoreContext } from '../contexts/StoreContext.js';
-import Article from './content_types/Article.js';
-import Book from './content_types/Book.js';
-import Organization from './content_types/Organization.js';
-import Video from './content_types/Video.js';
-import Footer from './Footer.js'
 
 // Full page of content. Content ID guaranteed to be in param
 function FullPage() {
     let { contentId } = useParams();
-    let { content } = useContext(StoreContext);
-    let thisContent = content.find(resource => resource.id === contentId);
-    let contentComp;
 
-    switch(thisContent.type) {
-        case "article":
-            contentComp = <Article content={thisContent}/>;
-            break;
-        case "book":
-            contentComp = <Book content={thisContent}/>;
-            break;
-        case "video":
-            contentComp = <Video content={thisContent}/>;
-            break;
-        case "organization":
-            contentComp = <Organization content={thisContent}/>;
-            break;
-        default:
-            return <div>Something has gone catostrophically wrong</div>
-    }
     return (
-        <div>
-            <div className="container">
-                {contentComp}
-            </div>
-            <Footer contentId={contentId}/>
-        </div>
-    )
+        <p>Show full page of content with id {contentId}</p>
+    );
 }
 
 export default FullPage;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import css from "./Header.module.css";
 
 function Header() {
+  
+  const history = useHistory();
+  
+  function handleReturn(){
+    history.goBack();
+  }
+  
+  function handleSearch(){
+    history.goBack();
+  }
+
+
     return (
-        <p>Header works!</p>
+        <div className={css.navbar} >
+               
+                <Link to="/home">
+                  <img src = "https://cdn.glitch.com/21330e98-ab0c-4abb-a9d3-3a74bd7d9c62%2Fsearch_icon.svg?v=1619456498681" alt = 'return' onClick={handleSearch} className={css.icon}/>
+                </Link>
+            
+                <Link to="/home">
+                  <button className={css.navbutton}>
+                    Home
+                  </button>
+                </Link>
+                
+                <img src = "https://cdn.glitch.com/21330e98-ab0c-4abb-a9d3-3a74bd7d9c62%2Freturn.svg?v=1619456932041" alt = 'return' onClick={handleReturn} className={css.icon}/>         
+        </div>
     );
 }
 
