a9446155e79da329bc1aa112d530ddf891ce56e5
Patrickberzins <67074061+Patrickberzins@users.noreply.github.com>
2021-05-02 18:03:33 -0400

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { StoreContext } from '../contexts/StoreContext.js';
+import css from "./About.module.css";
+
 
 function About() {
+  
+  let {
+	about
+  } = useContext(StoreContext);
+  
     return (
-        <div>
-            <p>About us works!</p>
-            <button>
-                <Link to="/home">
-                    Home
-                </Link>
-            </button>
+        <div className={css.container}> 
+            <h1 className={css.title}>ABOUT US</h1>
+            <p className={css.title}>{about.body}</p>
+            <p className={css.title}>Enjoy!</p>
         </div>
     )
 }
