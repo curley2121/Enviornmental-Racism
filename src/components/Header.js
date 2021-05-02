d7ef176303cfcd92538098a3466a2b8563fa7693
Patrickberzins <67074061+Patrickberzins@users.noreply.github.com>
2021-05-02 18:06:17 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import css from "./Header.module.css";
 
 function Header() {
+  
+  const history = useHistory();
+  
+  function handleReturn(){
+    history.goBack();
+  }
+  
+  function handleSearch(){
+    history.goBack();
+  }
+
+
     return (
-        <p>Header works!</p>
+        <div className={css.navbar} >
+               
+                <Link to="/Content/all">
+                  <img src = "./assets/search_icon.svg" alt = 'return'  className={css.icon}/>
+                </Link>
+            
+                <Link to="/home">
+                  <button className={css.navbutton}>
+                    Home
+                  </button>
+                </Link>
+                
+                <img src = "./assets/return.svg" alt = 'return' onClick={handleReturn} className={css.icon}/>         
+        </div>
     );
 }
 
