2c75e727bf69a476eaca5a540606d30b526856ed
co1010 <colinclarke1010@gmail.com>
2021-05-03 01:07:12 -0400

    fix pics

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useHistory } from "react-router-dom";
 import { Link } from 'react-router-dom';
 import css from "./Header.module.css";
+import publicUrl from '../utils/publicUrl';
 
 function Header() {
   
@@ -21,7 +22,7 @@ function Header() {
                
             
                 <Link to="/home">
-                  <img src = "./assets/EnviroLogo.svg" alt = 'Home'  className={css.icon}/>
+                  <img src = {publicUrl("/assets/EnviroLogo.svg")} alt = 'Home'  className={css.icon}/>
                 </Link>
                 
                 <img src = "./assets/return.svg" alt = 'Return' onClick={handleReturn} className={css.icon}/>         
