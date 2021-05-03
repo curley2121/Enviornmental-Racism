96b8dc7bd9688c4e22a1153ab5c3d40e0ad93ee3
co1010 <colinclarke1010@gmail.com>
2021-05-03 00:47:19 -0400

    comments and likes implemented

--- src/contexts/StoreContext.js
+++ src/contexts/StoreContext.js
@@ -9,8 +9,42 @@ export const StoreContext = createContext();
 
 function StoreContextProvider(props){
     const [store, setStore] = useState(initialStore);
+
+    function addLike(contentId){
+        const like = {
+            userId: store.currentUserId, 
+            contentId,
+            datetime: new Date().toISOString()
+        };
+        setStore({
+            ...store,
+            likes: store.likes.concat(like)
+        });
+    }
+        
+    function removeLike(contentId){
+        const newLikes = store.likes.filter(like=>!(like.userId===store.currentUserId && like.contentId===contentId));
+        setStore({
+            ...store,
+            likes: newLikes
+        });
+    }
+        
+    function addComment(contentId, text){
+        const comment = {
+            userId: store.currentUserId, 
+            contentId,
+            text,
+            datetime: new Date().toISOString()
+        };
+        setStore({
+            ...store,
+            comments:store.comments.concat(comment)
+        });
+    }
+
 	return (
-        <StoreContext.Provider value = {{...store}}>
+        <StoreContext.Provider value = {{...store, addComment, addLike, removeLike}}>
             {props.children}
         </StoreContext.Provider >
     ) 
--- src/utils/initialStore.js
+++ src/utils/initialStore.js
@@ -1,4 +1,5 @@
-export default { 
+export default {
+    currentUserId: "judy",
     about: {
         title: "Evironmental Racism",
         body: "Our goal is to collect and facilitate access to books, videos, websites, and academic papers covering environmental racism across the globe. In our research we quickly discovered that the topic is not as widely known as it should be and so we set out to provide an all in one resource to help educate the world. With your help we can move one step closer to making real progress in the fight against environmental racism so get reading!",
